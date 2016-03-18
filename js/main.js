c181cd08eaeb5949f63d84dcaf354ef02e27b5e9
SarahTierney <tierney.s@live.com>
2016-03-18 09:08:37 -0400

    Added code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-var $ball = $('.ball');
+var $body = $('body');
+
+$('html').on('keydown',function() {
+  var $ball = $('<div>');
+  $body.append($ball);
+  var newLeft = Math.random() * 100;
+  $ball.css('left', newLeft);
+  $ball.addClass('ball');
+});
+
+var $body = $('body');
+$('html').on('keydown', function() {
+  var $ball = $('<div>');
+  $body.append($ball);
+  var newLeft = Math.random() * 100;
+  $ball.css('left', newLeft);
+  $ball.addClass('ball');
+});
